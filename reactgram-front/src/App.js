5364bf71179b04b03e8b7c81aa2369272935c7b2
Rafael Reis <rafyangtj@gmail.com>
2025-12-20 18:00:06 -0300

    added-Navbar-Footer

--- reactgram-front/src/App.js
+++ reactgram-front/src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+//pages
 import Home from './pages/Home/Home';
 import Login from './pages/Auth/Login';
-import Register from './pages/Auth/Login'
+import Register from './pages/Auth/Register';
+
+//components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
    return (
       <div className="App">
          <div>
             <BrowserRouter>
+               <Navbar />
                <Routes>
                   <Route path="/" element={<Home />} />
                   <Route path="/login" element={<Login />} />
                   <Route path="/register" element={<Register />} />
                </Routes>
+               <Footer />
             </BrowserRouter>
          </div>
       </div>
