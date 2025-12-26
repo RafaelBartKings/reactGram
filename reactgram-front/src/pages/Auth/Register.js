a98a8a12b0b90e1d64c1e1c73a6d344bbcd3017b
Rafael Reis <rafyangtj@gmail.com>
2025-12-26 16:09:49 -0300

    Message

--- reactgram-front/src/pages/Auth/Register.js
+++ reactgram-front/src/pages/Auth/Register.js
@@ -2,6 +2,7 @@ import './Auth.css';
 
 // Components
 import { Link } from 'react-router-dom';
+import Message from '../../components/Message';
 
 // Hooks
 import { useState, useEffect } from 'react';
@@ -70,7 +71,9 @@ const Register = () => {
                onChange={e => setConfirmPassord(e.target.value)}
                value={passwordConfirmation || ''}
             />
-            <input type="submit" value="Cadastrar" />
+            {!loading && <input type="submit" value="Cadastrar" />}
+            {loading && <input type="submit" value="Aguarde..." disabled />}
+            {error && <Message msg={error} type="error" />}
          </form>
          <p>
             Já tem conta? <Link to="/login">Clique aqui.</Link>
